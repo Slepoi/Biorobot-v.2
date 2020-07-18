fe2939d506c37bf415a752cd1f9d2c67bc428da5
Slepoi <Slepois@mail.ru>
2020-07-19 02:01:55 +0700

    Add files via upload

---
+++ Biorobots/js/script.js
@@ -0,0 +1,16 @@
+new Vue({
+  el: '#app',
+  data: {
+  	gold: 0,
+    item1: 0,
+    item2: 0,
+    item3: 0
+  }
+}) 
+
+
+
+
+document.getElementById('close').onclick = function close (){
+document.getElementById('mone').style.display = 'none';
+}
